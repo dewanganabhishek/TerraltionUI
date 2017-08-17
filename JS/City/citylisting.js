bb58d762ead790b4e667be9371e9f246b7677c43
abhishek <abhishek.dewangan@holidayme.com>
2017-08-17 15:46:45 +0530

    Added Edit Functionality

--- JS/City/citylisting.js
+++ JS/City/citylisting.js
@@ -2,6 +2,20 @@
 $(document).ready(function () {
 
     var isProcessing = false;
+    $(document).on("click", ".editCity", function (e) {
+        var cityId = $(this).closest('.cityEdit').find('.cityId').text();
+        getCityDetails(cityId);
+    });
+
+    $(document).on("change", "#ddlCountry", function () {
+        $(this).val();
+        populateStateDropdDown("en", 3, $(this).val());
+
+    });
+
+    $(document).on("click", "#btnCityCancel", function (e) {
+        cancelEdit();
+    });
 
     $('.prev').on('click', function () {
         loadingTable('example');
@@ -72,8 +86,8 @@ $(document).ready(function () {
                 var htmlResult = "";
                 $('.datatableContent').html('');
                 for (var i = 0; i < result.data.length; i++) {
-                    htmlResult = htmlResult + "<tr>";
-                    htmlResult = htmlResult + "<td class='sorting_1'>" + result.data[i].Id + "</td>";
+                    htmlResult = htmlResult + "<tr class='cityEdit'>";
+                    htmlResult = htmlResult + "<td class='sorting_1 cityId'>" + result.data[i].Id + "</td>";
                     htmlResult = htmlResult + "<td class='title-width'>" + result.data[i].Title + '</td>';
                     htmlResult = htmlResult + '<td>' + result.data[i].StateTitle + '</td>';
                     htmlResult = htmlResult + '<td>' + result.data[i].CountryTitle + '</td>';
@@ -84,7 +98,7 @@ $(document).ready(function () {
                         htmlResult = htmlResult + "<td><input type='checkbox' checked='checked' ></td>";
                     }
 
-                    //htmlResult = htmlResult + '<td></td>';
+                    htmlResult = htmlResult + '<td><span style="cursor:pointer" class="editCity glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;<span style="cursor:pointer" class="glyphicon glyphicon-globe"></span></td>';
                     htmlResult = htmlResult + '</tr>';
                     $('#example').append(htmlResult);
                     htmlResult = "";
@@ -100,13 +114,50 @@ $(document).ready(function () {
                         "paging": false,
                         "processing": true,
                         "serverSide": false,
-                        "searching": false
+                        "searching": false,
+                        "bInfo": false
                     });
                 isProcessing = false;
             }).catch(function (result) {
                 alert("Fail");
             });
 
+    }
+
+    function getCityDetails(cityid) {
+
+        var apigClient = apigClientFactory.newClient();
+        var body = {};
+        var params = {
+            fields: "all",
+            id: Number(cityid),
+            languagecode: "en"
+        };
+        var additionalParams = {};
+        apigClient.cityDetailsGet(params, body, additionalParams)
+            .then(function (result) {
+            console.log(result);
+                $('#cityTitle').val(result.data.Title);
+                $('#displayTitle').val(result.data.CountryTitle + ", " + result.data.Title);
+                $('#lattitude').val(result.data.Latitude);
+                $('#longitude').val(result.data.Longitude);
+
+                $('#example').hide(); $('.w3-bar').hide();
+                $('#dataTables_info').hide();
+                $('.divEdit').show();
+                $('#isActive').prop('checked', result.data.IsActive);
+                $("#altertnateNames").tagsinput('removeAll');
+                $.each(result.data.AlternetNames, function (index, value) {
+                    $('#altertnateNames').tagsinput('add', value);
+                });
+
+                $('#ddlCountryId').val(result.data.CountryId);
+                $('#ddlStateId').val(result.data.StateId);
+                
+                populateCountryDropdDown("en", 2, cityid);
+                populateStateDropdDown("en", 3, 0);
+
+            });
     }
 
     function loadingTable(divId) {
@@ -126,6 +177,70 @@ $(document).ready(function () {
         $('#' + divId).append(html);
         $('.table-loading-overlay').css('height', $('#' + divId).height() + 'px');
         $('.table-loading-inner').css('padding-top', ($('#' + divId).height() / 2) + 'px');
+    }
+
+    function cancelEdit() {
+        $('#example').show(); $('.w3-bar').show();
+        $('#dataTables_info').show();
+        $('.divEdit').hide();
+    }
+
+    function populateCountryDropdDown(languageCode, regiontype, regionId) {
+
+        $("#ddlCountry").html('');
+        $("#ddlCountry").append($("<option></option>").val('-1').html('--Select--'));
+
+        var countrylist = populateRegionDropDown(languageCode, regiontype, regionId);
+        if (countrylist != null) {
+            countrylist.forEach(function (e) {
+                if (Number(e.Id) === Number($('#ddlCountryId').val())) {
+                    $("#ddlCountry").append($("<option selected=true></option>").val(e.Id).html(e.Title_en));
+                } else {
+                    $("#ddlCountry").append($("<option></option>").val(e.Id).html(e.Title_en));
+                }
+            });
+        }
+    }
+
+
+    function populateStateDropdDown(languageCode, regiontype, regionId) {
+        $("#ddlState").html('');
+        $("#ddlState").append($("<option selected=true></option>").val('-1').html('--Select--'));
+        
+        var citylist = populateRegionDropDown(languageCode, regiontype, regionId);
+        if (citylist != null) {
+            citylist.forEach(function (e) {
+                if (Number(e.Id) === Number($('#ddlStateId').val())) {
+                    $("#ddlState").append($("<option selected=true></option>").val(e.Id).html(e.Title_en));
+                } else {
+                    $("#ddlState").append($("<option></option>").val(e.Id).html(e.Title_en));
+                }
+            });
+        }
+    }
+
+    function populateRegionDropDown(languageCode, regiontype, regionId) {
+
+        var baseUrl = "https://5ztzfhb2f6.execute-api.ap-south-1.amazonaws.com/Dev/lookup?";
+        var region = Number(regiontype) == 1 ? "continent" : regiontype == 2 ? "country" : regiontype == 3 ? "state" : "city";
+        var data = [{}];
+        //alert(baseUrl + "region=" + region + "&regionId=" + regionId + "&languagecode=" + languageCode);
+
+        $.ajax({
+            url: baseUrl + "region=" + region + "&regionId=" + regionId + "&languagecode=" + languageCode,
+            cache: false,
+            type: "GET",
+            async : false,
+            success: function (response) {
+                if (response.RegionList != null) {
+                   data = response.RegionList;
+                } 
+            },
+            error: function (xhr) {
+                console.log("error:" + xhr);
+            }
+        });
+        return data;
     }
 
 });
